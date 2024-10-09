bb14b74ab3d137d80ca0de867cec651d539ab81e
“AminChebbi’’ <medaminchebbi20@gmail.com>
2024-10-09 16:44:33 +0200

    v4.3

--- JS_TP1/test.js
+++ JS_TP1/test.js
@@ -11,3 +11,45 @@ function test_age() {
             document.body.style.backgroundColor = "green";
         } 
 }
+function test_couleur() {
+    let couleur = prompt ("donner une couleur");
+    if (couleur == "rouge") {
+        document.body.style.backgroundColor ="red";
+    }
+    else if (couleur == "bleu") {
+        document.body.style.backgroundColor ="blue";
+    }
+    else if (couleur == "vert") {
+        document.body.style.backgroundColor ="green";
+    }
+    else {
+        alert("couleur non comprise");
+    }
+
+}
+function affichage() {
+    let prenom = prompt("saisir votre prenom");
+    let nom = prompt("saisir votre nom");
+    document.write("<div style='background-color : yellow; border:5px solid blue; width: 400px; height: auto; margin: auto; font-size:2em;'>")
+    document.write ("bonjour "+ prenom + "  " + nom);
+    document.write ("</div>");
+}
+function calcul_moyenne() {
+    var n1 = prompt("Donner la premiere note: ")
+    var n2 = prompt("Donner la deuxième note: ")
+    var n3 = prompt("Donner la troisième note: ")
+
+    var somme = Number(n1) + Number(n2) + Number(n3);
+
+    document.write("voici la somme : " + somme + "<br>");
+    var moyenne = somme/3;
+    let moyenneFormatee = moyenne.toFixed(2);
+    document.write("voici la moyenne : " + moyenneFormatee + "<br>");
+    
+    if (moyenne>10) {
+        document.write("ADMIS");
+    }
+    else {
+        document.write("REFUSER")
+    }
+}
